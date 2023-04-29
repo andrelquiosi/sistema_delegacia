c349d3fe8bd846a168da81d14955ac1af5d60fdd
andrelquiosi <andre.quiosi@Outlook.com>
2023-04-29 13:47:56 -0300

    renamed:    furtos.csv -> projeto_delegacia/furtos.csv 	modified:   projeto_delegacia/pom.xml 	new file:   projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/controle/persistencia/CsvToObjectReader.java 	modified:   projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java

--- projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java
+++ projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java
@@ -1,13 +1,19 @@
 package br.edu.utfpr.td.tsi.projeto_delegacia;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.edu.utfpr.td.tsi.projeto_delegacia.controle.persistencia.CsvToObjectReader;
 
 @SpringBootTest
 class ProjetoDelegaciaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException, ParseException {
+
 	}
 
 }
