c2004100a5430897c1cebcb76d5fb83676f9248d
andrelquiosi <andre.quiosi@Outlook.com>
2023-04-29 13:58:15 -0300

    modified:   projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/controle/persistencia/CsvToObjectReader.java 	modified:   projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java

--- projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/controle/persistencia/CsvToObjectReader.java
+++ projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/controle/persistencia/CsvToObjectReader.java
@@ -8,7 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
--- projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java
+++ projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java
@@ -1,19 +1,13 @@
 package br.edu.utfpr.td.tsi.projeto_delegacia;
 
-import java.io.IOException;
-import java.text.ParseException;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import br.edu.utfpr.td.tsi.projeto_delegacia.controle.persistencia.CsvToObjectReader;
 
 @SpringBootTest
 class ProjetoDelegaciaApplicationTests {
 
 	@Test
-	void contextLoads() throws IOException, ParseException {
-
+	void contextLoads() {
 	}
 
 }
