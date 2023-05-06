49be47dfe4651733a6a4c9f6ff8ce52ff884a548
andrelquiosi <andre.quiosi@Outlook.com>
2023-05-06 16:47:16 -0300

    mudança da pasta para a correta

--- projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/controle/persistencia/IFiltroBoletim.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.td.tsi.projeto_delegacia.controle.persistencia;
-
-import java.util.Date;
-
-public interface IFiltroBoletim {
-    public Date getDataOcorrencia();
-    public String getCidade();
-}
--- projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/controle/persistencia/IFiltroVeiculo.java
+++
@@ -1,6 +0,0 @@
-package br.edu.utfpr.td.tsi.projeto_delegacia.controle.persistencia;
-
-public interface IFiltroVeiculo {
-    public String getPlaca();
-    public String getMarca();
-}
