e0715493cf175121dde574ea1d2b6fc26748a9ac
Eduardo Bonatto <edubonatto10@gmail.com>
2023-05-09 20:08:24 -0300

    Criação da exceção de data de ocorrência

---
+++ projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/exceptions/DataOcorrenciaException.java
@@ -0,0 +1,7 @@
+package br.edu.utfpr.td.tsi.projeto_delegacia.exceptions;
+
+public class DataOcorrenciaException extends RuntimeException {
+    public DataOcorrenciaException(String message) {
+        super(message);
+    }
+}
